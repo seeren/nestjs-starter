2a7e3b4eaa7955318bcedd0fe0e40d442d643281
seeren <consultant@seeren.fr>
2022-08-22 21:36:16 +0200

    Remove webpack

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
-declare const module: any;
-
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 };
 
 bootstrap();
