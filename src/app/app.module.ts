91590bfe0043d03fc46107583c0cf746f12f62d0
seeren <consultant@seeren.fr>
2023-01-01 19:31:46 +0100

    Import example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { CoreModule } from './core/core.module';
 import { AppController } from './app.controller';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+import { FooModule } from './foo/foo.module';
 
 @Module({
-  imports: [SharedModule, CoreModule, AuthModule],
+  imports: [SharedModule, CoreModule, AuthModule, FooModule],
   controllers: [AppController],
 })
 export class AppModule {}
