516d885d1713bb2b451430dbe444b192ffc0245e
seeren <consultant@seeren.fr>
2023-01-09 22:10:46 +0100

    Remove AppController

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { CoreModule } from 'src/app/core/core.module';
-import { AppController } from 'src/app/app.controller';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AuthModule } from 'src/app/auth/auth.module';
 import { FooModule } from 'src/app/foo/foo.module';
 
 @Module({
   imports: [SharedModule, CoreModule, AuthModule, FooModule],
-  controllers: [AppController],
 })
 export class AppModule {}
