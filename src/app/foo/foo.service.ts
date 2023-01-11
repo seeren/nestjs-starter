5c5ff3fc11f057df9b5d4b61ff605a4a3fddcd00
seeren <consultant@seeren.fr>
2023-01-11 22:24:45 +0100

    Use insert instead of save

--- src/app/foo/foo.service.ts
+++ src/app/foo/foo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, Repository } from 'typeorm';
+import { DeleteResult, InsertResult, Repository } from 'typeorm';
 
 import { Foo } from 'src/app/foo/foo.entity';
 import { CreateFooDto } from 'src/app/foo/create-foo.dto';
@@ -16,12 +16,12 @@ export class FooService {
     return this.fooRepository.find();
   }
 
-  findOne(id: number): Promise<Foo | null> {
+  findOne(id: number): Promise<Foo> {
     return this.fooRepository.findOneBy({ id });
   }
 
-  async insert(fooDto: CreateFooDto): Promise<Foo> {
-    return this.fooRepository.save(fooDto);
+  async insert(fooDto: CreateFooDto): Promise<InsertResult> {
+    return this.fooRepository.insert(fooDto);
   }
 
   async delete(id: number): Promise<DeleteResult> {
