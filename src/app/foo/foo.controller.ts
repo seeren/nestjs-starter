269799667f664393cd63729e04c8ade6c6dc5198
seeren <consultant@seeren.fr>
2023-01-11 22:24:59 +0100

    Fix sgawer usage

--- src/app/foo/foo.controller.ts
+++ src/app/foo/foo.controller.ts
@@ -9,12 +9,18 @@ import {
   Post,
 } from '@nestjs/common';
 import { I18nService } from 'nestjs-i18n';
-import { ApiTags, ApiNotFoundResponse } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiNotFoundResponse,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
 
 import { Foo } from 'src/app/foo/foo.entity';
 import { FooService } from 'src/app/foo/foo.service';
 import { CreateFooDto } from 'src/app/foo/create-foo.dto';
 import { NotFoundHttpException } from 'src/app/core/exceptions/http/not-found-http.exception';
+import { FindOneByIdParams } from 'src/app/core/params/find-one-by-id.param';
+import { BadRequestHttpException } from 'src/app/core/exceptions/http/bad-request-http.exception';
 
 @ApiTags('Foo')
 @Controller('foos')
@@ -36,8 +42,11 @@ export class FooController {
    * Create a Foo
    */
   @Post()
-  async insert(@Body() fooDto: CreateFooDto): Promise<Foo> {
-    return this.fooService.insert(fooDto);
+  @ApiBadRequestResponse({ type: BadRequestHttpException })
+  async insert(@Body() fooDto: CreateFooDto): Promise<number> {
+    const insertResult = await this.fooService.insert(fooDto);
+
+    return insertResult.raw.insertId;
   }
 
   /**
@@ -45,8 +54,9 @@ export class FooController {
    */
   @Get(':id')
   @ApiNotFoundResponse({ type: NotFoundHttpException })
-  async findOne(@Param('id') id: number): Promise<Foo | null> {
-    const foo: Foo | null = await this.fooService.findOne(id);
+  @ApiBadRequestResponse({ type: BadRequestHttpException })
+  async findOne(@Param() params: FindOneByIdParams): Promise<Foo> {
+    const foo: Foo = await this.fooService.findOne(params.id);
 
     if (!foo) {
       throw new NotFoundException(await this.i18n.t('app.Foo not found'));
@@ -57,7 +67,13 @@ export class FooController {
 
   @Delete(':id')
   @HttpCode(204)
-  async delete(@Param('id') id: number): Promise<void> {
-    await this.fooService.delete(id);
+  @ApiNotFoundResponse({ type: NotFoundHttpException })
+  @ApiBadRequestResponse({ type: BadRequestHttpException })
+  async delete(@Param() params: FindOneByIdParams): Promise<void> {
+    const deleteResult = await this.fooService.delete(params.id);
+
+    if (!deleteResult.affected) {
+      throw new NotFoundException(await this.i18n.t('app.Foo not found'));
+    }
   }
 }
